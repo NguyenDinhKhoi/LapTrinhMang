e43a2eeb4bcae23bd9d3b51fe1a6fe26ebc483fe
NguyenDinhKhoi <NguyenDinhKhoi@DESKTOP-RN1HDET>
2018-03-23 23:01:49 +0700

    initial commit

---
+++ ChuyenDoiIp/LapTrinhAnToan1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LapTrinhAnToan1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bai 1 : Xac dinh ten may cuc bo + IP
+
+            //  String strHostName = Dns.GetHostName();
+            //  Console.WriteLine("Host Name : " + strHostName);
+
+            //  IPHostEntry iphost = Dns.GetHostByName(strHostName);
+
+            //  int ip = 0;
+            //  foreach(IPAddress ipadress in iphost.AddressList)
+            //  {
+            //      Console.WriteLine("IP : " + ++ip + ": " + ipadress.ToString());
+            //  }
+        
+
+            //Bai 2 : chuyen doi ten mien thanh Ip va nguoc lai
+            string str;
+            int a = -1;
+            Console.WriteLine("Nhap 1 neu muon chuyen doi ten mien, 2 neu muon chuyen doi IP");
+            a = int.Parse(Console.ReadLine());
+            if(a == 1)
+            {
+                Console.WriteLine("Nhap ten mien muon chuyen doi: ");
+                str = Console.ReadLine();
+                IPHostEntry iphost = Dns.GetHostByName(str);
+                foreach (IPAddress ipadress in iphost.AddressList)
+                {
+                    Console.WriteLine("IP : " + ipadress.ToString());
+                    return;
+                }
+            }
+            if(a==2)
+            {
+                Console.WriteLine("Nhap dia chi Ip muon chuyen doi thanh ten mien : ");
+                str = Console.ReadLine();
+                IPHostEntry iphost = Dns.Resolve(str);
+                Console.WriteLine("IP :" + iphost.HostName);
+                return;
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+        }
+    }
+}
---
+++ ServerOanTuXI/ServerOanTuXI/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ServerOanTuXI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint ipe = new IPEndPoint(IPAddress.Any, 3456);
+            sock.Bind(ipe);
+            EndPoint ep = ipe;
+
+            byte[] receiveData = new byte[10];
+            sock.ReceiveFrom(receiveData, ref ep);
+
+            int clientResult = Convert.ToInt32(Encoding.ASCII.GetString(receiveData));
+
+            Random r = new Random();
+            int ServerResult = r.Next(0, 3);
+
+            if ((ServerResult == 0 && clientResult == 0) || (ServerResult == 1 && clientResult == 1) || (ServerResult == 2 && clientResult == 2))
+            {
+                byte[] sendData = Encoding.ASCII.GetBytes("Hoa");
+                sock.SendTo(sendData, ep);
+                sock.Close();
+            }
+            else if ((ServerResult == 1 && clientResult == 0))
+            {
+                byte[] sendData = Encoding.ASCII.GetBytes("Thua");
+                sock.SendTo(sendData, ep);
+                sock.Close();
+            }
+            else if ((ServerResult == 2 && clientResult == 1))
+            {
+                byte[] sendData = Encoding.ASCII.GetBytes("Thua");
+                sock.SendTo(sendData, ep);
+                sock.Close();
+            }
+            else if ((ServerResult == 2 && clientResult == 0))
+            {
+                byte[] sendData = Encoding.ASCII.GetBytes("Thang");
+                sock.SendTo(sendData, ep);
+                sock.Close();
+            }
+            else if ((ServerResult == 1 && clientResult == 2))
+            {
+                byte[] sendData = Encoding.ASCII.GetBytes("Thang");
+                sock.SendTo(sendData, ep);
+                sock.Close();
+            }
+        }
+    }
+}
